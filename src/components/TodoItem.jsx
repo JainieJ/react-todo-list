88b42291ba4a7248b55ad82d229c94be0bfc9dfe
Janie <evgeniyaberseneva89@gmail.com>
2019-05-21 16:31:56 +0300

    Todo Item structure

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,7 +2,20 @@ import React, { Component } from "react";
 
 class TodoItem extends Component {
   render() {
-    return <h1>Hello from item</h1>;
+    const { handleDelete, handleEdit, title } = this.props;
+    return (
+      <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+        <h6>{title}</h6>
+        <div className="todo-icon">
+          <span className="mx-2 text-success" onClick={handleEdit}>
+            <i className="fas fa-pen" style={{ fontSize: "1rem" }} />
+          </span>
+          <span className="mx-2 text-danger" onClick={handleDelete}>
+            <i className="fas fa-trash" style={{ fontSize: "1rem" }} />
+          </span>
+        </div>
+      </li>
+    );
   }
 }
 
